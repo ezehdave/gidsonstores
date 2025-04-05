b5c23ce40bcf37777ac720207daabb8b3330c5cd
ezehdave <ezehudavid@gmail.com>
2025-04-05 05:00:00 +0100

    Added new_file_name.extension

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#dt#a!h(@bj0vkku=3sxmzvt7@nr3f7yw5)8lt!&p(j9m&@06p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
